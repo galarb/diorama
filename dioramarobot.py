012a3095fdfc4912fe387ad8d698988c0e7e2447
gal arbel <84223483+galarb@users.noreply.github.com>
2025-02-22 18:02:03 +0200

    dual core

--- dioramarobot.py
+++ dioramarobot.py
@@ -1,6 +1,7 @@
 from Diorama import Diorama
 from time import sleep
 from machine import Pin
+import _thread
 
 # Define motor settings
 encoder1_pin = 14
@@ -16,7 +17,7 @@ diorama = Diorama(
     speedup_pin=32, 
     speeddown_pin=33, 
     neo_pin=5, 
-    num_pix=8,
+    num_pix=62,
     encoder1_pin=encoder1_pin,
     encoder2_pin=encoder2_pin,
     in1_pin=in1_pin,
@@ -24,10 +25,24 @@ diorama = Diorama(
     wheel_size=wheel_size
 )
 
+# LED control task (Core 0)
+def neotask():
+    while True:
+        if diorama.get_button1_state():
+            diorama.rainbow()  # Active state LED effect
+        else:
+            diorama.heartbeat()  # Turn off LEDs when stopped
 
+# Motor control and button handling (Core 1)
+def main_task():
+    while True:
+        diorama.run()
+        sleep(0.01)
+        pass
 
-# Main Loop
-for _ in range(200):
-    diorama.run()
-    sleep(0.1)
+# Start the LED task on Core 0
+_thread.start_new_thread(neotask, ())
+
+# Run motor control on Core 1
+main_task()
 
