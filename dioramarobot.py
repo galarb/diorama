058f2d4ae8f4f2160fea57a513d9f266ef1cbafd
gal arbel <84223483+galarb@users.noreply.github.com>
2025-03-10 11:32:41 +0200

    two motors support

--- dioramarobot.py
+++ dioramarobot.py
@@ -3,26 +3,21 @@ from time import sleep
 from machine import Pin
 import _thread
 
-# Define motor settings
-encoder1_pin = 14
-encoder2_pin = 12
-in1_pin = 0
-in2_pin = 4
-wheel_size = 65
-
-# Create a Diorama instance with motor configuration
+# Create a Diorama instance with motors configuration
 diorama = Diorama(
     button1_pin=25, 
     button2_pin=26, 
-    speedup_pin=32, 
-    speeddown_pin=33, 
+    speedup_pin=33, 
+    speeddown_pin=32, 
     neo_pin=5, 
     num_pix=62,
-    encoder1_pin=encoder1_pin,
-    encoder2_pin=encoder2_pin,
-    in1_pin=in1_pin,
-    in2_pin=in2_pin,
-    wheel_size=wheel_size
+    encoder1_pin=12,
+    encoder2_pin=14,
+    in1b_pin=16,#base
+    in2b_pin=17,
+    in1d_pin=0,#disk
+    in2d_pin=4,
+    wheel_size=65
 )
 
 # LED control task (Core 0)
@@ -36,9 +31,9 @@ def neotask():
 # Motor control and button handling (Core 1)
 def main_task():
     while True:
+        diorama.motgo(90)
         diorama.run()
         sleep(0.01)
-        pass
 
 # Start the LED task on Core 0
 _thread.start_new_thread(neotask, ())
